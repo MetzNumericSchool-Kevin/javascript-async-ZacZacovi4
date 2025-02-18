28da707ea89fe5e139638a0349453e3eedd76242
KUKOBA Vadym <zaczacovi4@gmail.com>
2025-02-18 08:54:42 +0100

    exo 3 La Mission Temporelle Complexe

--- exercices.js
+++ exercices.js
@@ -10,6 +10,10 @@ const formChoixEpoqueHtml = document.querySelector(".form__choix_epoque");
 const formRechercheArtefact = document.querySelector(
   ".form__recherche_artefact"
 );
+const localisation_epoque_element = document.querySelector(
+  ".localisation_epoque"
+);
+const voyage_en_cours_element = document.querySelector(".voyage_en_cours");
 
 const epoques = {
   romaine: "Romaine",
@@ -31,6 +35,7 @@ function generationNombreAleatoireEntre(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Permet d'afficher l'époque de destination du voyage
 const afficherDestination = (nomEpoque) =>
   (localisationEpoqueHTML.textContent = nomEpoque);
 
@@ -52,7 +57,7 @@ formRechercheArtefact.addEventListener("submit", (event) => {
   const artefact = new FormData(formRechercheArtefact).get("artefact");
   quandRechercheArtefact(artefact);
 });
-
+// Permet d'afficher un artefact trouvée, ou non, à une époque
 const afficherRechercheArtefact = ({ artefact, epoque, success = true }) => {
   const li = document.createElement("li");
   li.textContent = `${success ? "✅" : "❌"} ${artefact} (Epoque ${epoque})`;
@@ -71,60 +76,119 @@ creerLesChoixEpoque(epoques);
 function quandEpoqueChoisie(nomEpoque) {
   nomEpoqueActuelle = nomEpoque;
   // Utilisation de votre fonction voyagerTemps
-  const localisation_epoque_element = document.querySelector('.localisation_epoque')
-  const voyage_en_cours_element = document.querySelector('.voyage_en_cours');
-  
-  localisation_epoque_element.style.display = 'none';
-  voyage_en_cours_element.style.display = 'block';
-  
-  voyagerTemps(nomEpoque, function () {
-    localisation_epoque_element.style.display = 'block';
-    voyage_en_cours_element.style.display = 'none';
+
+  localisation_epoque_element.style.display = "none";
+  voyage_en_cours_element.style.display = "inline-block";
+
+  voyagerTemps(nomEpoque, function (nomEpoque) {
+    localisation_epoque_element.style.display = "inline-block";
+    voyage_en_cours_element.style.display = "none";
     afficherDestination(nomEpoque);
-  })
+  });
 }
 
 // Fonction appelée plus haut quand le formulaire de recherche d'artefact est soumis
 function quandRechercheArtefact(artefact) {
   // Utilisation de votre fonction collecterArtefact
-  collecterArtefact(artefact, function(success, artefact){
-    if(success){
-      console.log("artefact été trouvé ", artefact)
-    }else{
-      console.log("artefact n'est pas trouvé ", artefact)
+  collecterArtefact(artefact, function (artefact) {
+    if (Math.random() > 0.5) {
+      afficherRechercheArtefact({
+        artefact: artefact,
+        epoque: nomEpoqueActuelle,
+        success: true,
+      });
+    } else {
+      afficherRechercheArtefact({
+        artefact: artefact,
+        epoque: nomEpoqueActuelle,
+        success: false,
+      });
+      // ? afficherRechercheArtefact({
+      //     artefact,
+      //     epoque: nomEpoqueActuelle,
+      //     success: true,
+      //   })
+      // : afficherRechercheArtefact({
+      //     artefact,
+      //     epoque: nomEpoqueActuelle,
+      //     success: false,
+      //   });
     }
-  })
+  });
 }
 
 //exo 1 Le Téléporteur Temporel
 
-function voyagerTemps(destination, callback){
-  console.log("Voyage en cours vers : ", destination)
+function voyagerTemps(destination, callback) {
+  console.log("Voyage en cours vers : ", destination);
   setTimeout(() => {
-    console.log("Arrive à destination : ", destination)
-    callback()
+    console.log("Arrive à destination : ", destination);
+    callback(destination);
   }, generationNombreAleatoireEntre(1000, 3000));
-};
-
+}
 
 // exo 2 La Collecte d'Artefact Mystère
 
-function collecterArtefact(nomArtefact, callback){
-  console.log("Recherche artefact ", nomArtefact)
+function collecterArtefact(nomArtefact, callback) {
+  console.log("Recherche artefact: ", nomArtefact);
   setTimeout(() => {
-    const chanceGainArtedact = Math.random() * 100;
-    console.log(chanceGainArtedact);
-    if(chanceGainArtedact >= 50){
-      //console.log("artefact trouvé ", nomArtefact)
-      callback(true, nomArtefact);
-    }else{
-      //console.log("artefact n'est pas trouvé ", nomArtefact)
-      callback(false, nomArtefact);
-    }
+    callback(nomArtefact);
   }, generationNombreAleatoireEntre(1000, 3000));
-};
+}
 
+// exo 3 La Mission Temporelle Complexe
 
+function missionTemporelleComplexe() {
+  voyagerTemps(epoques.medievale, function () {
+    collecterArtefact("épée de chevalier", function () {
+      if (Math.random() > 0.5) {
+        console.log(
+          "Artefact 'épée de chevalier' à l'époque ",
+          epoques.medievale,
+          " trouvé!"
+        );
+      } else {
+        console.log(
+          "Artefact 'épée de chevalier' à l'époque ",
+          epoques.medievale,
+          " n'étais pas trouvé!"
+        );
+      }
+      voyagerTemps(epoques.romaine, function () {
+        collecterArtefact("bouclier romain", function () {
+          if (Math.random() > 0.5) {
+            console.log(
+              "Artefact 'bouclier romain' à l'époque ",
+              epoques.medievale,
+              " trouvé!"
+            );
+          } else {
+            console.log(
+              "Artefact 'bouclier romain' à l'époque ",
+              epoques.medievale,
+              " n'étais pas trouvé!"
+            );
+          }
+          collecterArtefact("épée romaine", function () {
+            if (Math.random() > 0.5) {
+              console.log(
+                "Artefact 'épée romaine' à l'époque ",
+                epoques.medievale,
+                " trouvé!"
+              );
+            } else {
+              console.log(
+                "Artefact 'épée romaine' à l'époque ",
+                epoques.medievale,
+                " n'étais pas trouvé!"
+              );
+            }
+            console.log("Mission Temporelle Complexeterminée!");
+          });
+        });
+      });
+    });
+  });
+}
 
-
-
+missionTemporelleComplexe();
